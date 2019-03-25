bfd81479001c03ebf9ec46c5fd22a2f9e033bf7c
gandikotamadan <madanpro321@gmail.com>
2019-03-25 12:48:27 +0530

    major

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyA_71A6gnDoVuc-M3VfUnroz1aBb17ccgc",
+    authDomain: "movmadan.firebaseapp.com",
+    databaseURL: "https://movmadan.firebaseio.com",
+    projectId: "movmadan",
+    storageBucket: "movmadan.appspot.com",
+    messagingSenderId: "304060159295"
+  }
+};
